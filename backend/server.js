1d22a9be521516b6ce29c54cfd97ba4a3b296abd
Shriya Avhad <avhadshriya@gmailcom>
2026-01-02 11:43:34 +0530

    app.get

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/contact", require("./routes/contactRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 } else {
